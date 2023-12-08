bcd56cf2fd3dc432e176a36f2661cb3c6f25638f
Varya Utkina <varya@icloud.com>
2023-12-08 21:08:42 +0300

    Initial Commit

---
+++ HW 2/ViewControllers/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  HW 2
+//
+//  Created by Alexey Efimov on 12.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 15
+        setColor()
+        
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: redSlider.value.cgFloat(),
+            green: greenSlider.value.cgFloat(),
+            blue: blueSlider.value.cgFloat(),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
+extension Float {
+    func cgFloat() -> CGFloat {
+        CGFloat(self)
+    }
+}
