e365654f0fd04a37c45a1714e1d4505d46f9c69f
Varya Utkina <varya@icloud.com>
2023-12-09 10:16:50 +0300

    Setup ColorViewController with Navigation bar

---
+++ HW 2/ViewControllers/ColorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ColorViewController.swift
+//  HW 2
+//
+//  Created by Варвара Уткина on 09.12.2023.
+//  Copyright © 2023 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
